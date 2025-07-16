329e7a83299f714a9d3246bd4150b20c6e9cc0a4
ahm455 <ahmedgoharkiyani@gmail.com>
2025-07-16 23:58:39 +0500

    Create model.py

---
+++ model.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class ShortURL(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(500), nullable=False)
+    short_code = db.Column(db.String(10), unique=True, nullable=False)
+    access_count = db.Column(db.Integer, default=0)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'url': self.original_url,
+            'shortCode': self.short_code,
+            'createdAt': self.created_at.isoformat() if self.created_at else None,
+            'updatedAt': self.updated_at.isoformat() if self.updated_at else None,
+            'accessCount': self.access_count
+        }
